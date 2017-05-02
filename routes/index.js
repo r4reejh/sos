efd114a82167f6d13439dc241996205905f28b1c
Reejh Ghosh <Reejh Ghosh>
2017-05-02 17:34:31 +0530

    almost final

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,18 @@ router.get('/', function(req, res, next) {
   res.send('api');
 });
 
+router.get('/SOS/:lat/:long',(req,res)=>{
+	console.log(req.params.lat+" "+req.params.long);
+	coreController.SOS(req.params.lat,req.params.long)
+	.then((hit)=>{
+		res.status(200);
+		res.send({'hit':hit[0],'relief':hit[1],'status':'hello'});
+	})
+	.catch((e)=>{
+		console.log(e);
+	});
+});
+
 router.post('/SOS',(req,res)=>{
 	console.log(req.body);
 	coreController.SOS(req.body.lat,req.body.long)
